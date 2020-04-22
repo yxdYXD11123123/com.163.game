0faa9ee8c5985fc7467b0411ae1868c9a3e3e242
yxd11123123 <yuanxudong.201@qq.com>
2020-04-22 23:30:01 +0800

    项目第二天

--- 网易游戏官网（仿）/js/index.js
+++ 网易游戏官网（仿）/js/index.js
@@ -33,3 +33,224 @@ function closeGameNav() {
     // 并且给 顶部列表按钮添加类名
     $(".topnav-listbtn").removeClass("open")
 }
+
+/**
+ *  头部渲染数据  轮播 and Tab切换 and 新闻 and banner栏
+ */
+
+// 将轮播图数据遍历到页面中
+$.each(carouselList, function (i, j) {
+    $(".header-carousel ul").append(`
+   <li style="${j.bg}">
+      <a href="#" class="container">
+          <div class="header-carousel-content ${j.color}">
+              <h2>${j.h2}</h2>
+              <h3>${j.h3}</h3>
+              <p>${j.p}</p>
+              <i>查看详情</i>
+          </div>
+      </a>
+  </li>
+    `)
+    // 插入小圆点
+    $(".header-carousel ol").append(`<li></li>`)
+})
+// 第一个小圆点添加active
+$(".header-carousel ol li").eq(0).addClass("active")
+
+// 渲染tab切换内容
+$.each(tabList, function (i, j) {
+    $(".header-tab-left ul").append(`
+    <li class="header-tab-left-li">
+    <img src="${j.lilpicture}" alt="">
+    <div>
+        <h4>${j.title}</h4>
+        <p>${j.detail}</p>
+    </div>
+    <span></span>
+</li>
+    `)
+    $(".header-tab-left-content").append(`
+    <a href="javascript:;"><img src="${j.bigpicture}" title="${j.title}"></a>
+    `)
+})
+// 第一个li添加 current
+$(".header-tab-left ul li:nth-child(1)").addClass("current")
+
+$.each(newsList, function (i, j) {
+    $(".header-news ul").append(`
+    <li><a href="javascript:;">${j.title}</a> <span>${j.date}</span></li>
+    `)
+})
+
+// 将banner栏数据渲染进页面
+$.each(bannerList, function (i, j) {
+    $(".banner ul").append(`
+    <li><a href="javascript:;"><img src="${j.imgSrc}" alt=""></a> <span style="background-color: ${j.spanColor};"></span></li>
+    `)
+})
+
+
+/**
+ *  热门游戏动画渲染数据
+ */
+// 渲染一批 热门游戏
+// 记录每次从哪个数据开始渲染  （ 进入页面从 一个数据开始渲染 ） 
+let mainHotGameIndex = 0
+// 记录上次最后渲染到的数据
+let endindex = 0
+renderHotGameList()
+// 写一个可以循环，遍历热门游戏数据的功能函数  功能主要是实现：如果后面的数据不够的情况下，继续从头补充数据
+function renderHotGameList() {
+    // 使用 for 循环遍历数据
+    // 判断：我们计划要渲染出6个li    如果我们后面的数据还够6个
+    if (mainHotGameIndex + 5 <= mainHotGameList.length - 1) {
+        // 那么正常的接着渲染6个
+        for (let i = mainHotGameIndex; i < mainHotGameIndex + 6; i++) {
+            $(".main-hot-content").append(`
+             <li index="${i}">
+             <a href="#" class="main-hot-upload"><img src="${mainHotGameList[i].contentImg}" alt=""><img src="${mainHotGameList[i].uploadImg}" alt=""></a>
+             <h3>${mainHotGameList[i].h3}</h3>
+           <p>${mainHotGameList[i].p}</p>
+           <a href="#" class="main-hot-enter">进入官网</a>
+       </li>       
+        `)
+            // 并记录下最后一个渲染出的数据 的 索引
+            endindex = i
+        }
+    }
+    // 但是如果我们的数据已经不够6个 
+    else {
+        // 计算出  还差几组数据   6 - 还剩的数据组数( 例如：有6组数据   这次要从索引5开， 先遍历了索引5，还差5组，也就是 6-6-5   )
+        let difnum = 6 - (mainHotGameList.length - mainHotGameIndex)
+        for (let i = mainHotGameIndex; i < mainHotGameList.length; i++) {
+            $(".main-hot-content").append(`
+             <li>
+             <a href="#" class="main-hot-upload"><img src="${mainHotGameList[i].contentImg}" alt=""><img src="${mainHotGameList[i].uploadImg}" alt=""></a>
+             <h3>${mainHotGameList[i].h3}</h3>
+           <p>${mainHotGameList[i].p}</p>
+           <a href="#" class="main-hot-enter">进入官网</a>
+       </li>       
+        `)
+        }
+        // 接着利用 计算出的差值， 从头开始渲染数据，作为补充 ： 补充到数据生成6个li
+        for (let j = 0; j < difnum; j++) {
+            $(".main-hot-content").append(`
+        <li>
+        <a href="#" class="main-hot-upload"><img src="${mainHotGameList[j].contentImg}" alt=""><img src="${mainHotGameList[j].uploadImg}" alt=""></a>
+        <h3>${mainHotGameList[j].h3}</h3>
+      <p>${mainHotGameList[j].p}</p>
+      <a href="#" class="main-hot-enter">进入官网</a>
+  </li>       
+   `)
+            endindex = j
+        }
+    }
+    // 将结尾的数据记录下来， 以便于下次从下一个数据开始渲染
+    mainHotGameIndex = endindex + 1
+}
+
+animateHotGameLi()
+// 将已有的数据 热门游戏的 li 用动画的方式 依次放大显示
+function animateHotGameLi() {
+    // 这里的动画在 改变transform属性 ： 所以 需要一个让jquery的animate兼容transform的插件
+    // 利用延时器  结合  for循环实现，让6个li 依次间隔一定时间开始放大，
+    for (let i = 0; i < 6; i++) {
+        // 也就生成6个延时器，依次是 延时 100ms  200ms  300ms .......  也就是每个li的动画效果间隔了100ms
+        setTimeout(function () {
+            // 每个延时器到时间后就让 对应的li 开始动画效果
+            $(".main-hot-content li").eq(i).animate({
+                transform: "scale(1)",
+                opacity: 1
+            }, 1000)
+        }, i * 200)
+    }
+}
+
+
+
+/**
+ *  头部轮播图  轮播功能
+ */
+// 声明一个节流阀
+let flag = true
+// 声明一个变量记录小圆点的索引
+let circle = 0
+// 给小圆点绑定点击事件
+$(".header-carousel ol").on("click", "li", clickCircles)
+function clickCircles() {
+    if (flag == true) {
+        // 关闭节流阀
+        flag = false;
+        // 切换小圆点样式
+        $(this).addClass("active").siblings().removeClass("active");
+        // 记录点击索引
+        circle = $(this).index();
+        $(".header-carousel ul li").eq(circle).fadeIn(1200, function () {
+            // 控制节流阀
+            flag = true;
+        }).siblings().fadeOut(1200)
+        // 清楚定时器，重新计时
+        clearInterval(timer)
+        timer = setInterval(autoPlay, 5000)
+    }
+}
+// 写一个定时器，让轮播图定时轮播
+let timer = setInterval(autoPlay, 5000)
+function autoPlay() {
+    circle++
+    if (circle >= $(".header-carousel ol").children().length) {
+        circle = 0
+    }
+    // 切换小圆点样式
+    $(".header-carousel ol li").eq(circle).addClass("active").siblings().removeClass("active");
+    $(".header-carousel ul li").eq(circle).fadeIn(1200, function () {
+        // 控制节流阀
+        flag = true;
+    }).siblings().fadeOut(1200)
+}
+
+/**
+ * 头部 tab切换功能
+ */
+$('.header-tab-left ul').on("click", "li", clickTabLi)
+function clickTabLi() {
+    // 给被点击的li添加 .current样式
+    $(this).addClass("current").siblings().removeClass("current")
+    let $index = $(this).index()
+    $(".header-tab-left-content a").eq($index).stop().fadeIn(600).siblings().stop().fadeOut(600)
+}
+
+/**
+ *  main主体部分的热门游戏  换一批功能
+ */
+$(".main-hot-title span").on("click", clickChangeHot)
+function clickChangeHot() {
+    // 这里的动画在 改变transform属性 ： 所以 需要一个让jquery的animate兼容transform的插件
+    // 利用延时器  结合  for循环实现，让6个li 依次间隔一定时间开始 缩小/透明，
+    for (let i = 0; i < 6; i++) {
+        // 也就生成6个延时器，依次是 延时 100ms  200ms  300ms .......  也就是每个li的动画效果间隔了100ms
+        setTimeout(function () {
+            // 每个延时器到时间后就让 对应的li 开始动画效果
+            $(".main-hot-content li").eq(i).animate({
+                transform: "scale(0)",
+                opacity: 0
+            }, 1000, function () {
+                // 完全缩小，透明后，也就是动画结束后，清除自己
+                if ($(this).index() == 5) {
+                    // 如果这里是第六个li 已经完成动画  清空 ul
+                    $(".main-hot-content").empty()
+                }
+                // 清除自己后，看看 ul 里面还有没有元素，如果已经什么都没有了，就可以开始渲染新数据了
+                if ($(".main-hot-content").children().length == 0) {
+                    console.log($(".main-hot-content").children().length);
+                    // 渲染新数据 （后面6个数据）
+                    renderHotGameList()
+                    // 将新数据用动画展示出来
+                    animateHotGameLi()
+                }
+            })
+        }, i * 200)
+    }
+}
+
--- 网易游戏官网（仿）/js/list.js
+++ 网易游戏官网（仿）/js/list.js
@@ -674,3 +674,206 @@ let newGameList = [
         link: "#"
     }
 ]
+
+// 轮播图数据
+let carouselList = [
+    {
+        h2: "立夏集盛大开幕",
+        h3: "撸串斗蛋领海量福利",
+        p: "立夏集活动现已开启，完成节日任务，免费兑换挂件倒流壶；享用立夏美食，参加称重大赛，赢取丰厚奖励；每日领取激萌斗蛋，开启酷炫对决，可获得珍稀宝箱！",
+        bg: "background-image: url(./images/index/header_carousel_01.jpg)",
+        color: "color-white"
+    },
+    {
+        h2: "跨服擂台系统上线",
+        h3: "试剑天下，谁与争锋",
+        p: "玩家可以通过这一系统，与其他服务器的玩家进行对战。在这个舞台上，玩家可以以单人或团队的形式加入战斗，寻找最适合自己的战术，并在其中赢得荣耀，让更多人见证自己的实力！",
+        bg: "background-image: url(./images/index/header_carousel_02.jpg)",
+        color: ""
+    },
+    {
+        h2: "梦幻西游三维版",
+        h3: "新门派新主角 今日上线",
+        p: "次世代3D即时制手游《梦幻西游三维版》，在延续经典梦幻的同时，更塑造了一个万物有灵的西游大世界：开放剧情百种选择、仙魔精怪自由结交、万里疆域隐藏彩蛋，更有跨服赛事火热打响！",
+        bg: "background-image: url(./images/index/header_carousel_03.jpg)",
+        color: ""
+    },
+    {
+        h2: "“花与剑”新版来袭！",
+        h3: "鲜衣怒马，少年仗剑天涯",
+        p: "鲜衣怒马，少年仗剑天涯。这是在唐朝，这里有一段你的故事！欢迎加入这花有情、剑有意的“花与剑”江湖....",
+        bg: "background-image: url(./images/index/header_carousel_04.jpg)",
+        color: ""
+    },
+    {
+        h2: "荒野行动",
+        h3: "全新赛季S11启程",
+        p: "黑客入侵，未来战争一触即发！",
+        bg: "background-image: url(./images/index/header_carousel_05.jpg)",
+        color: "color-white"
+    }
+]
+
+// tab切换数据
+let tabList = [
+    {
+        title: "一梦江湖立夏集开幕",
+        detail: "撸串斗蛋，领永久挂件",
+        lilpicture: "./images/index/header_tab_li_01.png",
+        bigpicture: "./images/index/header_tab_content_01.jpg"
+    },
+    {
+        title: "跨服擂台系统上线",
+        detail: "与其他服务器玩家进行对战",
+        lilpicture: "./images/index/header_tab_li_02.png",
+        bigpicture: "./images/index/header_tab_content_02.jpg"
+    },
+    {
+        title: "《梦幻西游三维版》",
+        detail: "新门派新主角 今日上线",
+        lilpicture: "./images/index/header_tab_li_03.png",
+        bigpicture: "./images/index/header_tab_content_03.jpg"
+    },
+    {
+        title: "花与剑新版来袭",
+        detail: "鲜衣怒马，少年仗剑天涯",
+        lilpicture: "./images/index/header_tab_li_04.png",
+        bigpicture: "./images/index/header_tab_content_04.jpg"
+    },
+    {
+        title: "荒野行动",
+        detail: "全新赛季S11启程",
+        lilpicture: "./images/index/header_tab_li_05.png",
+        bigpicture: "./images/index/header_tab_content_05.jpg"
+    }
+]
+
+// 新闻数据   （只能由5个）
+let newsList = [
+    {
+        title: "满心如愿，《梦幻西游》手游第二期许愿树活动暖心开启",
+        date: "04-21"
+    },
+    {
+        title: "少年再会长安 大话IP动画《大话之少年游》开启预约",
+        date: "04-21"
+    },
+    {
+        title: "《隐世录》重磅曝光：NPC颜值超标警报！",
+        date: "04-21"
+    },
+    {
+        title: "Creeper？——《一起优诺》UNO Minecraft魔性升级",
+        date: "04-21"
+    },
+    {
+        title: "《实况球会经理》联动传奇球星伊涅斯塔！巅峰“小白”点燃绿茵！",
+        date: "04-21"
+    },
+    {
+        title: "衣钵相传，《轩辕剑龙舞云山》师徒系统上线",
+        date: "04-21"
+    },
+]
+
+// banner栏数据  (只能有4个)
+let bannerList = [
+    {
+        imgSrc: "./images/index/banner_01.png",
+        spanColor: "#ea4040"
+    },
+    {
+        imgSrc: "./images/index/banner_02.png",
+        spanColor: "#ea4040"
+    },
+    {
+        imgSrc: "./images/index/banner_03.jpg",
+        spanColor: "#ea4040"
+    },
+    {
+        imgSrc: "./images/index/banner_04.jpg",
+        spanColor: "white"
+    }
+]
+
+// main主体部分  热门游戏的数据
+let mainHotGameList = [
+    {
+        contentImg: "./images/index/main_hot_content_01.jpg",
+        uploadImg: "./images/index/main_hot_upload_01.png",
+        h3: "《一梦江湖》",
+        p: "享立夏美食，参加称重大赛，开启斗蛋对决，领永久挂件、珍稀宝箱等海量福利！",
+    },
+    {
+        contentImg: "./images/index/main_hot_content_02.jpg",
+        uploadImg: "./images/index/main_hot_upload_02.png",
+        h3: "《一梦江湖》",
+        p: "享立夏美食，参加称重大赛，开启斗蛋对决，领永久挂件、珍稀宝箱等海量福利！",
+    },
+    {
+        contentImg: "./images/index/main_hot_content_03.jpg",
+        uploadImg: "./images/index/main_hot_upload_03.jpg",
+        h3: "《一梦江湖》",
+        p: "享立夏美食，参加称重大赛，开启斗蛋对决，领永久挂件、珍稀宝箱等海量福利！",
+    },
+    {
+        contentImg: "./images/index/main_hot_content_04.jpg",
+        uploadImg: "./images/index/main_hot_upload_04.png",
+        h3: "《一梦江湖》",
+        p: "享立夏美食，参加称重大赛，开启斗蛋对决，领永久挂件、珍稀宝箱等海量福利！",
+    },
+    {
+        contentImg: "./images/index/main_hot_content_05.jpg",
+        uploadImg: "./images/index/main_hot_upload_05.png",
+        h3: "《一梦江湖》",
+        p: "享立夏美食，参加称重大赛，开启斗蛋对决，领永久挂件、珍稀宝箱等海量福利！",
+    },
+    {
+        contentImg: "./images/index/main_hot_content_06.jpg",
+        uploadImg: "./images/index/main_hot_upload_06.png",
+        h3: "《一梦江湖》",
+        p: "享立夏美食，参加称重大赛，开启斗蛋对决，领永久挂件、珍稀宝箱等海量福利！",
+    },
+    {
+        contentImg: "./images/index/main_hot_content_07.jpg",
+        uploadImg: "./images/index/main_hot_upload_07.png",
+        h3: "《一梦江湖》",
+        p: "享立夏美食，参加称重大赛，开启斗蛋对决，领永久挂件、珍稀宝箱等海量福利！",
+    },
+    {
+        contentImg: "./images/index/main_hot_content_08.jpg",
+        uploadImg: "./images/index/main_hot_upload_08.png",
+        h3: "《一梦江湖》",
+        p: "享立夏美食，参加称重大赛，开启斗蛋对决，领永久挂件、珍稀宝箱等海量福利！",
+    },
+    {
+        contentImg: "./images/index/main_hot_content_09.jpg",
+        uploadImg: "./images/index/main_hot_upload_09.png",
+        h3: "《一梦江湖》",
+        p: "享立夏美食，参加称重大赛，开启斗蛋对决，领永久挂件、珍稀宝箱等海量福利！",
+    },
+    {
+        contentImg: "./images/index/main_hot_content_10.jpg",
+        uploadImg: "./images/index/main_hot_upload_10.jpg",
+        h3: "《一梦江湖》",
+        p: "享立夏美食，参加称重大赛，开启斗蛋对决，领永久挂件、珍稀宝箱等海量福利！",
+    },
+    {
+        contentImg: "./images/index/main_hot_content_11.png",
+        uploadImg: "./images/index/main_hot_upload_11.png",
+        h3: "《一梦江湖》",
+        p: "享立夏美食，参加称重大赛，开启斗蛋对决，领永久挂件、珍稀宝箱等海量福利！",
+    },
+    {
+        contentImg: "./images/index/main_hot_content_12.jpg",
+        uploadImg: "./images/index/main_hot_upload_12.png",
+        h3: "《一梦江湖》",
+        p: "享立夏美食，参加称重大赛，开启斗蛋对决，领永久挂件、珍稀宝箱等海量福利！",
+    },
+    {
+        contentImg: "./images/index/main_hot_content_13.jpg",
+        uploadImg: "./images/index/main_hot_upload_13.png",
+        h3: "《一梦江湖》",
+        p: "享立夏美食，参加称重大赛，开启斗蛋对决，领永久挂件、珍稀宝箱等海量福利！",
+    }
+]
